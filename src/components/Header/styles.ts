c186db798f9f1696df699f216583b6973afda8b9
Vini <vini@teste>
2025-07-15 17:31:49 -0300

    finalizando projeto

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,45 +1,88 @@
 import styled from 'styled-components'
+
 import { cores } from '../../styles'
 
-export const Header = styled.header`
+export const Produto = styled.div``
+
+export const Titulo = styled.h3`
+  min-height: 64px;
+`
+
+export const Capa = styled.div`
+  position: relative;
+
+  img {
+    display: block;
+    width: 100%;
+    margin-bottom: 8px;
+  }
+`
+
+export const Prices = styled.div`
+  margin: 16px 0;
+  color: #fff;
+
+  small {
+    font-size: 16px;
+    text-decoration: line-through;
+    margin-right: 8px;
+    opacity: 0.7;
+  }
+
+  strong {
+    font-size: 18px;
+  }
+`
+
+export const Tag = styled.span`
+  background-color: ${cores.corPrincipal};
+  position: absolute;
+  top: 8px;
+  left: 8px;
+  padding: 8px;
+  font-size: 12px;
+  font-weight: 700;
+  color: #fff;
+`
+export const Plataformas = styled.ul`
+  display: flex;
+  flex-wrap: wrap;
+  margin: 8px 0 0;
+
+  li {
+    background-color: ${cores.corPrincipal};
+    padding: 8px;
+    font-size: 10px;
+    white-space: nowrap;
+    margin-right: 8px;
+    margin-bottom: 8px;
+    color: #fff;
+    font-weight: bold;
+  }
+`
+export const BtnComprar = styled.button`
+  display: block;
+  width: 100%;
   background-image: linear-gradient(
     45deg,
     ${cores.corPrincipal},
     ${cores.corSecundaria}
   );
-  margin: 80px 0;
-  padding: 16px 24px;
-  display: flex;
-  border-radius: 6px;
-  align-items: center;
+  font-weight: bold;
+  font-size: 16px;
+  padding: 12px 0;
+  text-transform: capitalize;
+  color: #eee;
+  border: none;
+  cursor: pointer;
+  border-radius: 4px;
+  margin-bottom: 8px;
 
-  h1 {
-    font-size: 18px;
-    flex: 1;
-    font-style: italic;
-    color: ${cores.corFundo};
-  }
-
-  div {
-    display: flex;
-    align-items: center;
-
-    img {
-      width: 18px;
-      margin-right: 8px;
-      margin-left: 16px;
-    }
-
-    span {
-      color: ${cores.corFundo};
-    }
-  }
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-
-    div {
-      margin-top: 16px;
-    }
+  &:hover {
+    background-image: linear-gradient(
+      -45deg,
+      ${cores.corPrincipal},
+      ${cores.corSecundaria}
+    );
   }
 `
