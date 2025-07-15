2da37be460770c53511e75e15ef41a65976bac6d
Vini <vini@teste>
2025-07-15 17:48:05 -0300

    atualiza reportWebVitals para versão recente do web-vitals

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,13 @@
-import { ReportHandler } from 'web-vitals'
+import { onCLS, onLCP, onFCP, onTTFB, Metric } from 'web-vitals'
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry)
-      getFID(onPerfEntry)
-      getFCP(onPerfEntry)
-      getLCP(onPerfEntry)
-      getTTFB(onPerfEntry)
-    })
+const reportWebVitals = (onPerfEntry?: (metric: Metric) => void) => {
+  if (onPerfEntry && typeof onPerfEntry === 'function') {
+    onCLS(onPerfEntry)
+    onLCP(onPerfEntry)
+    onFCP(onPerfEntry)
+    onTTFB(onPerfEntry)
   }
 }
 
 export default reportWebVitals
+export { reportWebVitals }
